271a52502700b0a4da06025e2ccc6e83d7a0bfae
Anndres <silvabasauri@gmail.com>
2024-09-29 09:55:17 -0500

    Se arregla de como se muestra en el datatable el campo fecha

--- admin/vistas/scripts/asistencia.js
+++ admin/vistas/scripts/asistencia.js
@@ -1,78 +1,78 @@
 var tabla;
 
 // Función que se ejecuta al inicio
-function init(){
-   listar();
-   listaru();
+function init() {
+    listar();
+    listaru();
 
-   // Manejo de formularios
-   $("#formulario").on("submit",function(e){
-      guardaryeditar(e);
-   });
+    // Manejo de formularios
+    $("#formulario").on("submit", function (e) {
+        guardaryeditar(e);
+    });
 
-   // Cargamos los items al select cliente
-   $.post("../ajax/asistencia.php?op=selectPersona", function(r){
-      // Agregar opción "Todos" al inicio del select
-      var todosOption = '<option value="">Todos</option>';
-      $("#idcliente").html(todosOption + r); // Concatenar la opción "Todos" con los demás resultados
-      $('#idcliente').selectpicker('refresh'); // Refrescar el selectpicker para actualizar
-   });
+    // Cargamos los items al select cliente
+    $.post("../ajax/asistencia.php?op=selectPersona", function (r) {
+        // Agregar opción "Todos" al inicio del select
+        var todosOption = '<option value="">Todos</option>';
+        $("#idcliente").html(todosOption + r); // Concatenar la opción "Todos" con los demás resultados
+        $('#idcliente').selectpicker('refresh'); // Refrescar el selectpicker para actualizar
+    });
 }
 
 
 
 
 //funcion listar
-function listar(){
-	tabla=$('#tbllistado').dataTable({
-		"aProcessing": true,//activamos el procedimiento del datatable
-		"aServerSide": true,//paginacion y filrado realizados por el server
-		dom: 'Bfrtip',//definimos los elementos del control de la tabla
-		buttons: [ 
-                  'excelHtml5',
-                  'pdf'
-		],
-		"ajax":
-		{
-			url:'../ajax/asistencia.php?op=listar',
-			type: "get",
-			dataType : "json",
-			error:function(e){
-				console.log(e.responseText);
-			}
-		},
-		"bDestroy":true,
-		"iDisplayLength":10,//paginacion
-		"order":[[0,"desc"]]//ordenar (columna, orden)
-	}).DataTable();
+function listar() {
+    tabla = $('#tbllistado').dataTable({
+        "aProcessing": true,//activamos el procedimiento del datatable
+        "aServerSide": true,//paginacion y filrado realizados por el server
+        dom: 'Bfrtip',//definimos los elementos del control de la tabla
+        buttons: [
+            'excelHtml5',
+            'pdf'
+        ],
+        "ajax":
+        {
+            url: '../ajax/asistencia.php?op=listar',
+            type: "get",
+            dataType: "json",
+            error: function (e) {
+                console.log(e.responseText);
+            }
+        },
+        "bDestroy": true,
+        "iDisplayLength": 10,//paginacion
+        "order": [[0, "desc"]]//ordenar (columna, orden)
+    }).DataTable();
 }
-function listaru(){
-	tabla=$('#tbllistadou').dataTable({
-		"aProcessing": true,//activamos el procedimiento del datatable
-		"aServerSide": true,//paginacion y filrado realizados por el server
-		dom: 'Bfrtip',//definimos los elementos del control de la tabla
-		buttons: [                
-                  'excelHtml5',              
-                  'pdf'
-		],
-		"ajax":
-		{
-			url:'../ajax/asistencia.php?op=listaru',
-			type: "get",
-			dataType : "json",
-			error:function(e){
-				console.log(e.responseText);
-			}
-		},
-		"bDestroy":true,
-		"iDisplayLength":10,//paginacion
-		"order":[[0,"desc"]]//ordenar (columna, orden)
-	}).DataTable();
+function listaru() {
+    tabla = $('#tbllistadou').dataTable({
+        "aProcessing": true,//activamos el procedimiento del datatable
+        "aServerSide": true,//paginacion y filrado realizados por el server
+        dom: 'Bfrtip',//definimos los elementos del control de la tabla
+        buttons: [
+            'excelHtml5',
+            'pdf'
+        ],
+        "ajax":
+        {
+            url: '../ajax/asistencia.php?op=listaru',
+            type: "get",
+            dataType: "json",
+            error: function (e) {
+                console.log(e.responseText);
+            }
+        },
+        "bDestroy": true,
+        "iDisplayLength": 10,//paginacion
+        "order": [[0, "desc"]]//ordenar (columna, orden)
+    }).DataTable();
 }
 
 
 
-function listar_asistencia(){
+function listar_asistencia() {
     var fecha_inicio = $("#fecha_inicio").val();
     var fecha_fin = $("#fecha_fin").val();
     var idcliente = $("#idcliente").val();
@@ -98,12 +98,17 @@ function listar_asistencia(){
             {
                 "targets": 0, // Primera columna donde está la fecha
                 "render": function (data, type, row) {
-                    var date = new Date(data);
-                    var day = ("0" + date.getDate()).slice(-2);
-                    var month = ("0" + (date.getMonth() + 1)).slice(-2);
-                    var year = date.getFullYear();
-                    return day + "/" + month + "/" + year; // Formato DD/MM/AAAA
+                    if (data) {
+                        // Asumiendo que el formato que llega desde la base de datos es "YYYY-MM-DD"
+                        var parts = data.split("-"); // Dividir la fecha en partes
+                        var day = parts[2];  // Día
+                        var month = parts[1];  // Mes
+                        var year = parts[0];  // Año
+                        return day + "/" + month + "/" + year; // Formato DD/MM/AAAA
+                    }
+                    return data; // Si no hay dato, lo muestra tal cual
                 }
+
             },
             {
                 "targets": 3, // Columna de Fecha/Hora
@@ -123,32 +128,32 @@ function listar_asistencia(){
 }
 
 
-function listar_asistenciau(){
- var  fecha_inicio = $("#fecha_inicio").val();
- var fecha_fin = $("#fecha_fin").val();
+function listar_asistenciau() {
+    var fecha_inicio = $("#fecha_inicio").val();
+    var fecha_fin = $("#fecha_fin").val();
 
-	tabla=$('#tbllistado_asistenciau').dataTable({
-		"aProcessing": true,//activamos el procedimiento del datatable
-		"aServerSide": true,//paginacion y filrado realizados por el server
-		dom: 'Bfrtip',//definimos los elementos del control de la tabla
-		buttons: [
-                  'excelHtml5',
-                  'pdf'
-		],
-		"ajax":
-		{
-			url:'../ajax/asistencia.php?op=listar_asistenciau',
-			data:{fecha_inicio:fecha_inicio, fecha_fin:fecha_fin},
-			type: "get",
-			dataType : "json",
-			error:function(e){
-				console.log(e.responseText);
-			}
-		},
-		"bDestroy":true,
-		"iDisplayLength":10,//paginacion
-		"order":[[0,"desc"]]//ordenar (columna, orden)
-	}).DataTable();
+    tabla = $('#tbllistado_asistenciau').dataTable({
+        "aProcessing": true,//activamos el procedimiento del datatable
+        "aServerSide": true,//paginacion y filrado realizados por el server
+        dom: 'Bfrtip',//definimos los elementos del control de la tabla
+        buttons: [
+            'excelHtml5',
+            'pdf'
+        ],
+        "ajax":
+        {
+            url: '../ajax/asistencia.php?op=listar_asistenciau',
+            data: { fecha_inicio: fecha_inicio, fecha_fin: fecha_fin },
+            type: "get",
+            dataType: "json",
+            error: function (e) {
+                console.log(e.responseText);
+            }
+        },
+        "bDestroy": true,
+        "iDisplayLength": 10,//paginacion
+        "order": [[0, "desc"]]//ordenar (columna, orden)
+    }).DataTable();
 }
 
 
