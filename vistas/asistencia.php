693a2f733e75be4aee9cbdfbcc92eef1a7759236
Anndres <silvabasauri@gmail.com>
2024-10-12 00:05:23 -0500

    Se hace rollback

--- vistas/asistencia.php
+++ vistas/asistencia.php
@@ -6,145 +6,104 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Control | Asistencia</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-
-    <!-- Toastr CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="../admin/public/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../admin/public/css/font-awesome.css">
+    <!-- Estilo del tema -->
+    <link rel="stylesheet" href="../admin/public/css/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../admin/public/css/blue.css">
+    <link rel="shortcut icon" href="../admin/public/img/favicon.ico">
     <style>
         body {
-            background: linear-gradient(to bottom, #1d2b64, #f8cdda);
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
+            background: linear-gradient(to bottom, #3498db, #2c3e50);
+            color: #fff;
             margin: 0;
-            font-family: 'Arial', sans-serif;
         }
 
-        .login-container {
-            display: flex;
-            width: 800px;
-            background: white;
-            border-radius: 10px;
-            box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
-        }
-
-        .login-image {
-            flex: 1;
-            background: url('../admin/files/negocio/QT.png') no-repeat center center;
-            background-size: cover;
-            border-top-left-radius: 10px;
-            border-bottom-left-radius: 10px;
-            margin-left: 20px;
-        }
-
-        .login-form {
-            flex: 1;
-            padding: 40px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .login-form h2 {
-            margin-bottom: 20px;
-            font-size: 24px;
-            font-weight: 700;
-            color: #333;
-        }
-
-        .input-group {
-            display: flex;
-            align-items: center;
-            margin-bottom: 20px;
-            width: 100%;
-            position: relative;
-        }
-
-        .input-group input {
-            width: 100%;
-            padding: 12px 40px;
-            border: none;
-            border-radius: 30px;
-            background: #f2f2f2;
-            font-size: 16px;
-            color: #333;
-        }
-
-        .input-group i {
+        .lockscreen-wrapper {
+            max-width: 400px;
+            margin: 0 auto;
+            text-align: center;
+            padding: 60px;
+            box-sizing: border-box;
             position: absolute;
-            left: 15px;
-            font-size: 16px;
-            color: #aaa;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: #fff;
+            border-radius: 20px;
         }
 
-        .login-button {
-            width: 100%;
-            padding: 12px;
-            background: #333;
-            color: white;
-            border: none;
-            border-radius: 30px;
-            font-size: 16px;
-            cursor: pointer;
-            transition: background 0.3s ease;
-        }
-
-        .login-button:hover {
-            background: #555;
-        }
-
-        #current-time {
-            margin-top: 20px;
-            font-size: 24px;
-            color: #333;
-            font-weight: bold;
-        }
-
+        .lockscreen-logo,
+        .lockscreen-name,
+        .lockscreen-item,
+        .help-block,
         .lockscreen-footer {
-            margin-top: 20px;
             text-align: center;
         }
 
-        .lockscreen-footer a {
-            color: #3498db;
-            font-size: 16px;
+        .lockscreen-item {
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(160, 32, 240);
+            margin-top: 20px;
+        }
+
+        #current-time {
+            text-align: center;
+            padding: 10px;
+            border-radius: 5px;
+            font-size: 24px;
+            font-weight: bold;
+            margin-top: 20px;
+            color: #000000;
         }
     </style>
 </head>
 
-<body>
-    <div class="login-container">
-        <!-- Imagen de la izquierda -->
-        <div class="login-image"></div>
+<body class="hold-transition lockscreen">
 
-        <!-- Formulario de la derecha -->
-        <div class="login-form">
-            <h2>Control de Asistencia</h2>
-            <form action="" class="lockscreen-credentials" name="formulario" id="formulario" method="POST" onsubmit="return confirmarMarcado()">
-                <div class="input-group">
-                    <i class="fas fa-user"></i>
-                    <input type="text" class="form-control" name="codigo_persona" id="codigo_persona" placeholder="ID de asistencia">
-                </div>
-                <button type="submit" class="login-button">Marcar</button>
-            </form>
+    <div class="lockscreen-wrapper">
+        <?php //include '../ajax/asistencia.php' 
+        ?>
+        <div name="movimientos" id="movimientos"></div>
 
-            <!-- Mostrar la hora actual -->
-            <div id="current-time">00:00:00</div>
+        <div class="lockscreen-logo">
+            <a href="#"><b>Control</b> ASISTENCIA</a>
+        </div>
 
-            <div class="lockscreen-footer">
-                <a href="../admin/">Iniciar Sesión</a>
+        <div class="lockscreen-item">
+            <div class="lockscreen-image">
+                <img src="../admin/files/negocio/default.jpg" alt="User Image">
             </div>
+
+            <form action="" class="lockscreen-credentials" name="formulario" id="formulario" method="POST">
+                <div class="input-group">
+                    <input type="password" class="form-control" name="codigo_persona" id="codigo_persona" placeholder="ID de asistencia">
+                    <div class="input-group-btn">
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-arrow-right text-muted"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <!-- Mostrar la hora actual -->
+        <div id="current-time">00:00:00</div>
+
+        <div class="lockscreen-footer">
+            <a href="../admin/">Iniciar Sesión</a>
         </div>
     </div>
 
     <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
-    <!-- Toastr JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="../admin/public/js/jquery-3.1.1.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../admin/public/js/bootstrap.min.js"></script>
+    <!-- Bootbox -->
+    <script src="../admin/public/js/bootbox.min.js"></script>
 
     <!-- JavaScript para actualizar la hora -->
     <script>
@@ -158,30 +117,15 @@
             minutes = (minutes < 10 ? "0" : "") + minutes;
             seconds = (seconds < 10 ? "0" : "") + seconds;
 
-            document.getElementById("current-time").innerHTML = `<span>${hours}:${minutes}:${seconds}</span>`;
+            document.getElementById("current-time").innerHTML = `<span style="font-size: 24px; font-weight: bold;">${hours}:${minutes}:${seconds}</span>`;
             setTimeout(updateTime, 1000);
         }
 
         updateTime();
-
-        function confirmarMarcado() {
-            var currentTime = new Date();
-            var hours = currentTime.getHours();
-            var minutes = currentTime.getMinutes();
-            var seconds = currentTime.getSeconds();
-
-            hours = (hours < 10 ? "0" : "") + hours;
-            minutes = (minutes < 10 ? "0" : "") + minutes;
-            seconds = (seconds < 10 ? "0" : "") + seconds;
-
-            var horaMarcado = hours + ":" + minutes + ":" + seconds;
-
-            // Mostrar el toaster
-            toastr.success("Marcaste tu asistencia a las " + horaMarcado, "Confirmación");
-
-            return false; // Previene el envío real del formulario
-        }
     </script>
+
+    <!-- Tus otros scripts -->
+    <script type="text/javascript" src="scripts/asistencia.js"></script>
 
 </body>
 
